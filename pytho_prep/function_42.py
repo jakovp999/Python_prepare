b2d437ba1913e2d76cf86449b1e8a692269be8ed
jakovp999 <jakovp999@gmail.com>
2022-01-13 20:46:38 +1100

    сделал домашнюю работу ? урок

--- pytho_prep/function_42.py
+++ pytho_prep/function_42.py
@@ -102,3 +102,81 @@
 # print(list(map(lambda  x: x**2,numbers)))
 #
 # print(list(map(lambda  x: str(x), numbers)))
+
+#Домашнее задание
+
+# def person_info(name,age,city):
+#     result = (f'{name}, {age} год(а) проживает в {city}') # !!!!другой синтаксис не такой как на уроке
+#     return  result
+#
+# print(person_info ('Василий', 21, 'Москва'))
+# print(type(person_info))
+
+#поиск максимума
+# def get_max(a,b,c):
+#     result = max(a,b,c)
+#     return result
+# result = get_max(9,7,10)
+#
+# print(result)
+
+# #игра со здоровьем и жизнью
+#
+# player_name = input('Введите имя игрока')
+#
+# player = {
+#     'name': player_name,
+#     'health': 100,
+#     'damage': 50,
+# }
+#
+# enemy_name = input('Введите имя врага')
+#
+# enemy = {
+#     'name': enemy_name,
+#     'health': 50,
+#     'damage': 30,
+# }
+#
+# def attack(unit, target):
+#     target['health']-=unit['damage']
+#
+# attack (player, enemy)
+# print(enemy)
+#
+# attack(enemy, player)
+# print(player)
+
+#игра со здоровьем и жизнью  и броня
+
+player_name = input('Введите имя игрока')
+
+player = {
+    'name': player_name,
+    'health': 100,
+    'damage': 50,
+    'armor': 1.2
+}
+
+enemy_name = input('Введите имя врага')
+
+enemy = {
+    'name': enemy_name,
+    'health': 50,
+    'damage': 30,
+    'armor': 1.0
+}
+
+def get_damage(damage, armor):
+    return damage/ armor
+
+
+def attack(unit, target):
+    damage = get_damage(unit['damage'], target['armor'])
+    target['health'] -= damage
+
+attack (player, enemy)
+print(enemy)
+
+attack(enemy, player)
+print(player)
